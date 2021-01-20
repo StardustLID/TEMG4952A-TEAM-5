187f9c023cd3742030adc6c9dcec384dd34922be
Borisyu <hyyuag@connect.ust.hk>
2021-01-20 09:34:55 +0800

    Add Grid line for histogram

--- frontend/src/components/d3_charts/Histogram.js
+++ frontend/src/components/d3_charts/Histogram.js
@@ -1,12 +1,17 @@
 import * as d3 from "d3";
 
-// TODO: Total 3 Tasks, Reference: https://www.d3-graph-gallery.com/graph/histogram_basic.html
+// Reference: https://www.d3-graph-gallery.com/graph/histogram_basic.html
 
 const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 60, RIGHT: 20 };
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class Histogram {
+  /**
+   * @param element - Reference to the <div /> that the chart will be rendered in 
+   * @param {string} csvData - The CSV data file
+   * @param {string[]} axisLabels - The x-axis and y-axis labels
+   */
   constructor(element, csvData, axisLabels) {
     let vis = this;
 
@@ -46,57 +51,61 @@ export default class Histogram {
     vis.yAxisGroup = vis.svg.append("g");
 
     const data = d3.csvParse(csvData); // Parse a string of CSV data
-      console.log(data)
-      /** TODO: Task 3 Choose Which Max
-      *  if the maximum is too far from the general data, then choose 1000
-      *  Otherwise, use the d3.max function*/
         
-      let max = d3.max(data, (d) => d.x_values)
+    let max = d3.max(data, (d) => d.x_values)
+    var x = d3
+        .scaleLinear()
+        .domain([0, max * 1.1])
+        .range([0, WIDTH]);
 
-      var x = d3
-          .scaleLinear()
-          .domain([0, max * 1.1])
-          .range([0, WIDTH]);
+    //Call Axis
+    const xAxisCall = d3.axisBottom(x);
+    vis.xAxisGroup.transition().duration(500).call(xAxisCall);
 
-      //Call Axis
-      const xAxisCall = d3.axisBottom(x);
-      vis.xAxisGroup.transition().duration(500).call(xAxisCall);
-
-      // set the parameters for the histogram
-      // x.ticks divide the x.domain into different interval, ie. [0, 10), [10, 20), [20, 30) ...
-      // Thresholds are defined as an array of values [x0, x1, …]. Any value less than x0 will be placed in the first bin; 
-      // any value greater than or equal to x0 but less than x1 will be placed in the second bin; and so on. 
-      // Thus, the generated bins will have thresholds.length + 1 bins.
-      var histogram = 
-        d3
-        .bin()
-        .value((d) => {return +d.x_values;}) // I need to give the vector of value
-        .domain(x.domain()) // then the domain of the graphic
-        .thresholds(x.ticks(70)); // then the numbers of bins
-        
-      // And apply this function to data to get the bins
-      var bins = histogram(data);
+    // set the parameters for the histogram
+    // x.ticks divide the x.domain into different interval, ie. [0, 10), [10, 20), [20, 30) ...
+    // Thresholds are defined as an array of values [x0, x1, …]. Any value less than x0 will be placed in the first bin; 
+    // any value greater than or equal to x0 but less than x1 will be placed in the second bin; and so on. 
+    // Thus, the generated bins will have thresholds.length + 1 bins.
+    var histogram = 
+      d3
+      .bin()
+      .value((d) => {return +d.x_values;}) // I need to give the vector of value
+      .domain(x.domain()) // then the domain of the graphic
+      .thresholds(x.ticks(70)); // then the numbers of bins
       
-      // Y axis: scale and draw:
-      var y = d3.scaleLinear().range([HEIGHT, 0]);
-      y.domain([0,1.1 * d3.max(bins, function (d) {return d.length;})]); // d3.hist has to be called before the Y axis obviously
-      vis.yAxisGroup.transition().duration(500).call(d3.axisLeft(y));
-        
-      // append the bar rectangles to the svg element
-        
-      vis.svg
-        .selectAll("rect")
-        .data(bins)
-        .enter()
-        .append("rect")
-        .attr("x", (d)=>x(d.x0))
-        .attr("width",(d)=> x(d.x1) - x(d.x0) - 1)
-        .attr("fill", "#69b3a2")
-        .attr("y", HEIGHT)
-        .transition()
-        .duration(500)
-        .attr("height", (d) => HEIGHT - y(d.length))
-        .attr("y", (d)=>y(d.length));
+    // And apply this function to data to get the bins
+    var bins = histogram(data);
+    
+    // Y axis: scale and draw:
+    var y = d3.scaleLinear().range([HEIGHT, 0]);
+    y.domain([0,1.1 * d3.max(bins, function (d) {return d.length;})]); // d3.hist has to be called before the Y axis obviously
+    vis.yAxisGroup.transition().duration(500).call(d3.axisLeft(y));
+
+    /** Create horizontal grid lines (Ref: https://www.essycode.com/posts/adding-gridlines-chart-d3/)
+     * Passing the negative chart height and width to the tickSize functions ensures that the axis lines will span across the chart. 
+     * Passing an empty string to tickFormat ensures that tick labels aren’t rendered. 
+     * The ticks function specifies the number of tick marks, here set to 10 to equal the count on the main axes.
+     */
+    const yAxisGridCall = d3.axisLeft(y).tickSize(-WIDTH).tickFormat("").ticks(10);
+    vis.svg.append("g")
+      .attr("class", "axis-grid")
+      .call(yAxisGridCall);
+      
+    // append the bar rectangles to the svg element
+    vis.svg
+      .selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+      .attr("x", (d)=>x(d.x0))
+      .attr("width",(d)=> x(d.x1) - x(d.x0) - 1)
+      .attr("fill", "#69b3a2")
+      .attr("y", HEIGHT)
+      .transition()
+      .duration(500)
+      .attr("height", (d) => HEIGHT - y(d.length))
+      .attr("y", (d)=>y(d.length));
   }
 
   removeGraph() {
