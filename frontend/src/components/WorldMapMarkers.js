1d71754e899703a0da763b62f55dfb06ad469cb4
AnsonH <ansonheung2000@gmail.com>
2021-01-27 18:53:17 +0800

    Add more markers to the world map

--- frontend/src/components/WorldMapMarkers.js
+++ frontend/src/components/WorldMapMarkers.js
@@ -4,6 +4,7 @@ import topCitiesLocation from "./topCitiesLocation.json";
 import marker0_png from "../assets/map_marker0.png";
 import marker1_png from "../assets/map_marker1.png";
 import marker2_png from "../assets/map_marker2.png";
+import marker3_png from "../assets/map_marker3.png";
 
 export default function WorldMapMarkers(props) {
   const { countData } = props;
@@ -30,12 +31,19 @@ export default function WorldMapMarkers(props) {
     popupAnchor: [0, -30],
   });
 
+  const marker3 = L.icon({
+    iconUrl: marker3_png,
+    iconSize: [35, 35],
+    iconAnchor: [17, 34],
+    popupAnchor: [0, -30],
+  });
+
   const chooseMarker = (count) => {
     // prettier-ignore
     return count >= 8 ? marker0 :
-           count >= 5 ? marker1 :
-           count >= 3 ? marker2 :
-                        null;
+           count >= 4 ? marker1 :
+           count >  0 ? marker2 :
+                        marker3;
   };
 
   return topCitiesLocation.map((element) => {
